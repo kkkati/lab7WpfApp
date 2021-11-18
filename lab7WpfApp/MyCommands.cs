0c0b78326b35a5a14dd67ed84c5e2c7605d3c4f5
Екатерина Горяинова <kateas27@mail.ru>
2021-11-18 22:11:30 +0200

    Добавьте файлы проекта.

---
+++ lab7WpfApp/MyCommands.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace lab7WpfApp
+{
+    class MyCommands
+    {
+        
+        public static RoutedCommand Exit { get; set; }
+        public static RoutedCommand Open { get; set; }
+        public static RoutedCommand Save { get; set; }
+        static MyCommands()
+        {
+            InputGestureCollection inputs = new InputGestureCollection();
+            inputs.Add(new KeyGesture(Key.T, ModifierKeys.Control,"Ctrl+T"));
+            InputGestureCollection inputs2 = new InputGestureCollection();
+            inputs2.Add(new KeyGesture(Key.O, ModifierKeys.Control, "Ctrl+O"));
+            InputGestureCollection inputs3 = new InputGestureCollection();
+            inputs2.Add(new KeyGesture(Key.S, ModifierKeys.Control, "Ctrl+S"));
+            Exit = new RoutedCommand("Exit", typeof(MyCommands), inputs);
+            Open = new RoutedCommand("Open", typeof(MyCommands), inputs2);
+            Save = new RoutedCommand("Save", typeof(MyCommands), inputs3);
+        }
+    }
+}
